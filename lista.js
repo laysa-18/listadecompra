c561ee0ef30af24d7f3397267186835aa774c1f1
laysa-18 <129955793+laysa-18@users.noreply.github.com>
2023-05-25 11:08:17 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,13 @@
+let itens = document.getElementById("itens")
+function minhafuncao()
+{
+//criar um elemento li
+let li = document.createElement("li");
+//criar um texto
+let texto = document.createTextNode(intens.value);
+//adicionar um texto no elemento
+li.appendChild(texto)
+//adicionar o li na minhalista
+document.getElementById("minhalista").appendChild(li)
+
+}
